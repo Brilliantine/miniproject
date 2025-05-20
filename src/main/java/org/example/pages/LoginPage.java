c89d563715610d19f78a85da05b8ca8c293baed8
mefesto <tosha.g@mail.ru>
2025-05-20 17:43:42 +0300

    Добавил логику в LoginPage

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -1,4 +1,36 @@
 package org.example.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    private WebDriver driver;
+
+    private By usernameInput = By.id("username");
+    private By passwordInput = By.id("password");
+    private By loginButton = By.id("loginBtn");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get("https://example.com/login"); // замени на реальный URL
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameInput).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
 }
