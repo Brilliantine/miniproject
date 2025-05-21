4f270c48bd2876a2a08361c5ba20792753765799
Brilliantine <tosha.g@mail.ru>
2025-05-21 11:49:07 +0300

    Добавил нужные библеотеки в класс LoginTets и инициализировал WebDriver

--- src/main/java/org/example/tests/LoginTest.java
+++ src/main/java/org/example/tests/LoginTest.java
@@ -1,4 +1,10 @@
 package org.example.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.example.pages.LoginPage;
+
 public class LoginTest {
+    WebDriver driver = new FirefoxDriver();
+
 }
