f80cbfac99792f47cc96e2a820d449aa85d6f0e0
gainitdinovaa <tosha.g@mail.ru>
2025-05-21 13:42:13 +0300

    Добавил реализацию теста в LoginTest

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
     private WebDriver driver;
 
-    private By usernameInput = By.id("username");
-    private By passwordInput = By.id("password");
-    private By loginButton = By.id("loginBtn");
+    private By usernameInput = By.id("j_username");
+    private By passwordInput = By.id("j_password");
+    private By loginButton = By.className("btn btn-primary");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void open() {
-        driver.get("https://example.com/login"); // замени на реальный URL
+        driver.get("https://test-pass.rzd.ru/universal/logon/ru#BY_LOGIN");
     }
 
     public void enterUsername(String username) {
--- src/main/java/org/example/tests/LoginTest.java
+++ src/main/java/org/example/tests/LoginTest.java
@@ -1,10 +1,24 @@
 package org.example.tests;
 
+import org.example.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.example.pages.LoginPage;
 
 public class LoginTest {
-    WebDriver driver = new FirefoxDriver();
+    public static void main (String args[]){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+
+        loginPage.enterUsername("testovtestov");
+        loginPage.enterPassword("Qwerty123");
+        loginPage.clickLogin();
+
+        System.out.println("Заголовок страницы:" + loginPage.getTitle());
+
+        driver.quit();
+    }
 
 }
