fa2b670447c2cc00e52b16ad8f294da1b5343a9d
gainitdinovaa <tosha.g@mail.ru>
2025-05-24 15:44:00 +0300

    Создал константу baseUrl. В коде заменил адрес url на использование константы

--- src/main/java/org/example/pages/LinksPage.java
+++ src/main/java/org/example/pages/LinksPage.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class LinksPage {
     private WebDriver driver;
+    private final String baseUrl = "https://the-internet.herokuapp.com";
     private List<WebElement> links;
     private List<String> extLinks;
     private List<String> internLinks;
@@ -17,7 +18,7 @@ public class LinksPage {
         this.driver=driver;
     }
     public void open (){
-        driver.get("https://the-internet.herokuapp.com/");
+        driver.get(baseUrl);
     }
     public void searchLinks (){
         links=driver.findElements(By.tagName("a"));
@@ -36,7 +37,7 @@ public class LinksPage {
         extLinks = new ArrayList<>();
         for(WebElement link : links){
             String href = link.getAttribute("href");
-            if(href != null && !href.startsWith("https://the-internet.herokuapp.com")){
+            if(href != null && !href.startsWith(baseUrl)){
                 extLinks.add(href);
                 System.out.println("- "+href);
             }
@@ -52,7 +53,7 @@ public class LinksPage {
         internLinks = new ArrayList<>();
         for(WebElement link : links){
             String href = link.getAttribute("href");
-            if(href != null && href.startsWith("https://the-internet.herokuapp.com")){
+            if(href != null && href.startsWith(baseUrl)){
                 internLinks.add(href);
                 System.out.println("- "+href);
             }
