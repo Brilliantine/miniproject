06d389e39c7632a699f5051a2948fbe8ca52a04a
gainitdinovaa <tosha.g@mail.ru>
2025-05-24 15:35:16 +0300

    Отредактировал логику в LinksPage и LinksTest

--- src/main/java/org/example/pages/LinksPage.java
+++ src/main/java/org/example/pages/LinksPage.java
@@ -9,26 +9,59 @@ import java.util.*;
 public class LinksPage {
     private WebDriver driver;
     private List<WebElement> links;
+    private List<String> extLinks;
+    private List<String> internLinks;
     private Set<String> uniqueUrls = new HashSet<>();
 
     public LinksPage(WebDriver driver){
         this.driver=driver;
     }
-    public void open(){driver.get("https://the-internet.herokuapp.com/");}
-    public void searchLinks(){
-        links=driver.findElements(By.tagName("a"));
-        System.out.println("***Всего ссылок** "+links.size());
+    public void open (){
+        driver.get("https://the-internet.herokuapp.com/");
     }
-    public void printTextAndUrl(){
-        for (WebElement link : links){
-            System.out.println("***Текст: "+link.getText()+" | URL: "+link.getAttribute("href"));
-        }
+    public void searchLinks (){
+        links=driver.findElements(By.tagName("a"));
+    }
+    public int countLinks(){
+        return links.size();
     }
     public void uniqueLinks(){
-        for(WebElement link : links){
+        for (WebElement link : links){
             String href = link.getAttribute("href");
             if(href!=null){uniqueUrls.add(href);}
         }
-        System.out.println("***Уникальных URL: "+uniqueUrls.size());
+        System.out.println("***Уникальных ссылок: "+uniqueUrls.size());
+    }
+    public void printExternalLinks(){
+        extLinks = new ArrayList<>();
+        for(WebElement link : links){
+            String href = link.getAttribute("href");
+            if(href != null && !href.startsWith("https://the-internet.herokuapp.com")){
+                extLinks.add(href);
+                System.out.println("- "+href);
+            }
+        }
+        if(extLinks.isEmpty()){
+            System.out.println("Внешних ссылок на данной странице не обнаружено");
+        }
+    }
+    public int countExternalLinks(){
+        return extLinks.size();
+    }
+    public void printInternalLinks(){
+        internLinks = new ArrayList<>();
+        for(WebElement link : links){
+            String href = link.getAttribute("href");
+            if(href != null && href.startsWith("https://the-internet.herokuapp.com")){
+                internLinks.add(href);
+                System.out.println("- "+href);
+            }
+        }
+        if(internLinks.isEmpty()){
+            System.out.println("Внутренних ссылок на данной странице не отбнаружено");
+        }
+    }
+    public int countInternalLinks(){
+        return internLinks.size();
     }
 }
--- src/main/java/org/example/tests/LinksTest.java
+++ src/main/java/org/example/tests/LinksTest.java
@@ -15,46 +15,19 @@ public class LinksTest {
 
         LinksPage linksPage = new LinksPage(driver);
         linksPage.open();
-
         linksPage.searchLinks();
-        linksPage.printTextAndUrl();
+        int countLinks = linksPage.countLinks();
+        System.out.println("***Всего ссылок: "+countLinks);
         linksPage.uniqueLinks();
-        // 2. Получение всех ссылок
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("Всего ссылок: " + links.size());
+        System.out.println("***Внешние ссылки:");
+        linksPage.printExternalLinks();
+        System.out.println("***Внутренние ссылки: ");
+        linksPage.printInternalLinks();
+        int countExternalLinks = linksPage.countExternalLinks();
+        System.out.println("***Внешних ссылок: "+countExternalLinks);
+        int countInternalLinks = linksPage.countInternalLinks();
+        System.out.println("***Внутренних ссылок: "+countInternalLinks);
 
-
-        // 3. Вывод текста и href
-        for (WebElement link : links) {
-            System.out.println("Текст: " + link.getText() + " | URL: " + link.getAttribute("href"));
-        }
-
-        // 4. Уникальные ссылки (Set)
-        Set<String> uniqueUrls = new HashSet<>();
-        for (WebElement link : links) {
-            String href = link.getAttribute("href");
-            if (href != null) {
-                uniqueUrls.add(href);
-            }
-        }
-        System.out.println("Уникальных URL: " + uniqueUrls.size());
-
-        // 5. Карта текст -> href (Map)
-        Map<String, String> textToUrl = new HashMap<>();
-        for (WebElement link : links) {
-            String text = link.getText().trim();
-            String href = link.getAttribute("href");
-            if (!text.isEmpty() && href != null) {
-                textToUrl.put(text, href);
-            }
-        }
-
-        System.out.println("Карта 'текст -> URL':");
-        for (Map.Entry<String, String> entry : textToUrl.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
-
-        // Закрыть браузер
         driver.quit();
     }
 }
